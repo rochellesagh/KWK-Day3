63c625f2fcd94779be23b72baa29e934839306d6
Apple <apple@app.local>
2019-07-24 10:24:40 -0400

    Initial Commit

---
+++ 07_classes&objects.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+//var str = "Hello, playground"
+// This is the codefor the class
+//class Can {
+//    let containerType = "Can"
+//    var stuffInside : String
+//
+//    init(fruit : String){
+//        stuffInside = fruit
+//    }
+//    func description(){
+//        print("\(stuffInside) are packed inside this \(containerType)")
+//    }
+//    func description2(){
+//        print("\(stuffInside) is good for you")
+//    }
+//    func description3(){
+//        print("\(stuffInside) are kosher and healthy")
+//    }
+//}
+//
+////Code on line 20-22 are printing properties of our object
+//var cannedPeaches = Can(fruit : "peaches")
+//print(cannedPeaches)
+//print(cannedPeaches.containerType)
+//print(cannedPeaches.stuffInside)
+//
+////code on line 25 is calling action/method/function within the object
+//cannedPeaches.description()
+//cannedPeaches.description2()
+//cannedPeaches.description3()
+
+
+class Scholar {
+    let name = "Rochelle"
+    var karlie : String
+       init(name : String){
+           karlie = name
+        
+    }
+        
+    let grade = "College Sophmore"
+        var karlie : String
+    //come back to this
+        
+        init(grade : String){
+            karlie = grade
+        }
+    let studying = "Undecided"
+
+    func scholarClass(){
+        print("\(name) is a \(karlie)")
+    }
+}
+
